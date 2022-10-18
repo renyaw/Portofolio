5d4a7247a86691f7db6da72a695d926b30a70883
reikochn <norarenada@students.undip.ac.id>
2022-10-19 02:41:14 +0700

    all
    update total

---
+++ script.js
@@ -0,0 +1,13 @@
+// Start Scroll
+const sc = ScrollReveal({
+  origin: "top",
+  reset: true,
+  distance: "70px",
+  duration: 2000,
+  delay: 400,
+});
+
+// About
+sc.reveal(".foto", { delay: 400 });
+
+//
