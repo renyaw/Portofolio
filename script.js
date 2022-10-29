9e236887e23741d6dd9b28e665d7d458a54f3d0b
reikochn <norarenada@students.undip.ac.id>
2022-10-29 11:27:30 +0700

    replace about dll

--- script.js
+++ script.js
@@ -9,5 +9,23 @@ const sc = ScrollReveal({
 
 // About
 sc.reveal(".foto", { delay: 400 });
+sc.reveal(".hello", { delay: 500 });
+sc.reveal(".cv", { delay: 550 });
+sc.reveal(".desc", { delay: 800 });
+sc.reveal(".mail", { delay: 600 });
+sc.reveal(".git", { delay: 650 });
+sc.reveal(".ins", { delay: 700 });
+sc.reveal(".link", { delay: 750 });
+sc.reveal(".bout", { delay: 900 });
 
-//
+// Skill
+sc.reveal(".skill", { delay: 950 });
+sc.reveal(".skilld", { delay: 980 });
+sc.reveal(".html", { delay: 1050 });
+sc.reveal(".php", { delay: 1150 });
+sc.reveal(".py", { delay: 1250 });
+sc.reveal(".cl", { delay: 1350 });
+
+// Project 
+sc.reveal(".project", { delay: 1450 });
+sc.reveal(".project1", { delay: 1490 });
